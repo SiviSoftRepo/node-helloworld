ac22c0cce90bd4902f37c5902f7ccfb0f9131f75
SivisoftDevops <89481379+SivisoftDevops@users.noreply.github.com>
2021-09-09 10:28:05 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var http = require('http');
 
-var server = http.createServer(function(request, response) {
+var server1 = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.end("Hello World!");
@@ -8,6 +8,6 @@ var server = http.createServer(function(request, response) {
 });
 
 var port = 80;
-server.listen(port);
+server1.listen(port);
 
 console.log("Server running at http://localhost:%d", port);
